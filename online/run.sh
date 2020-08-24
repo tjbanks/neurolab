26bc8723f686291c626ed4e7e57b207d12d49e31
Tyler Banks <34382569+tjbanks@users.noreply.github.com>
2020-08-24 14:17:50 -0500

    Update run.sh

--- online/run.sh
+++ online/run.sh
@@ -15,7 +15,7 @@ cd $HOME
 
 mkdir Desktop 
 cd Desktop
-git clone https://github.com/tjbanks/Tutorials_S2-S6
+git clone https://github.com/chenziao/Software_Tutorial_S2-S6
 git clone https://github.com/chenziao/Computational-Neuroscience-Tutorials
 # git clone https://github.com/tjbanks/bmtool-cell-prefab
 # git clone https://github.com/tjbanks/two-cell-hco
